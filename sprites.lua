bb0a707fb32c7ac4780577f9d45f66b317694fd7
naomi <158738710+bobofromkirby64@users.noreply.github.com>
2026-07-08 00:29:28 -0700

    [Roundelie v0.8.0] ground-slam / shockwave rework
      - significantly reduced the range of ground-slam hitbox
          - reduced range for the big ground-slam from 60 -> 28 (7.5 -> 3.5 tiles)
          - reduced range for the small ground-slam from 40 -> 20 (5 -> 2.5 tiles)
          - the hitbox is further constrained to the width of the surface roundelie dives into, + 1/2 tile on each side
          - the hitbox no longer extends through walls
      - extended the active duration for both ground-slam hitboxes (from 1 -> 2f)
      - a big ground-slam now causes pieces of the ground to shoot out from the impact, and the hitbox moves up with them
          - the hitbox only extends after a short (2f) delay
          - the active duration of the hitbox is longer on a big ground-slam (2 -> 5f)
          - knockback is significantly reduced after the hitbox moves with the ground pieces
      - added new hitbox for shockwave(s)
          - two shockwaves are created after a big ground-slam, traveling out from the impact of the dive
          - one shockwave is created after a fully-angled small slam, traveling away from the direction of the bounce
          - the shockwave hitbox:
              - is only active after an initial (2f) delay
              - has relatively low knockback and damage
              - doesn't travel through walls
              - (unlike the ground-slam hitbox) isn't constrained by the width of the surface roundelie dives into
      - added placeholder visual effects for the updated ground-slam and shockwave
      - slightly reduced the amount of smoke drawn after a ground-slam

--- sprites.lua
+++ sprites.lua
@@ -39,6 +39,7 @@ local spriteList = {
     "heart",
     "adelie",
     {"smoke", 3, 1},
+    {"ground_chunk", 3, 1},
     {"characters/maddy_1", 7, 1},
     {"characters/maddy_2", 7, 1},
     {"characters/maddy_3", 7, 1},
